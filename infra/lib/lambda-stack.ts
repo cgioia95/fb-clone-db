bd470ce10cff46940ccb173eb72104def4783f0a
cgioia95 <christiang101@gmail.com>
2025-05-23 18:07:56 +1000

    auto transpile with nodjes function

--- infra/lib/lambda-stack.ts
+++ infra/lib/lambda-stack.ts
@@ -1,15 +1,17 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import * as path from "node:path";
 
 export class HelloLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const fn = new lambda.Function(this, "HelloLambdaStack", {
+
+    new NodejsFunction(this, "HelloLambdaStack", {
+      entry: path.join(__dirname, "../../src/lambda/index.ts"),
+      handler: "handler",
       runtime: lambda.Runtime.NODEJS_LATEST,
-      handler: "index.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "../../src/lambda")),
     });
   }
 }
